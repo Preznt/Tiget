ee925ad719e75b137d9c196b47f312d6b2f50a07
PollyGotACracker <bitterns96@gmail.com>
2022-11-20 19:42:05 +0900

    html, js 수정

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const calendar = document.querySelector("table.calendar");
+  const tbody = document.querySelector("table.calendar tbody");
   const btnPrev = document.querySelector("button.prev");
   const btnNext = document.querySelector("button.next");
   const btnToday = document.querySelector("button.today");
-  const btnSearchDetail = document.querySelector("button.btn_search_detail");
   const bgBlur = document.querySelector("div.bg_blur");
   const btnModalClose = document.querySelector("button.modal.btn_close");
 
@@ -60,27 +60,20 @@ document.addEventListener("DOMContentLoaded", () => {
      * dIndex.prev : 이번 달 첫 주에서 표시할 저번 달의 시작 날짜(prevLastDate - 이전 요일 Index)
      *               prevMonthDays에서 + 1 했으므로 - 1
      */
+
     const dIndex = {
       current: 1,
       prev: prevLastDate - (prevMonthDays - 1),
       next: 1,
     };
 
-    // 모든 td 안의 날짜 div(dateTxt) 제거
-    const tdAll = document.querySelectorAll("td");
-    for (let td of tdAll) {
-      let dateTxt = document.querySelector(".date_txt");
-      if (Array.from(td.children).includes(dateTxt)) {
-        td.removeChild(dateTxt);
-      }
-    }
+    tbody.textContent = "";
 
-    const trs = document.querySelectorAll("table.calendar tbody tr");
     for (let j = 0; j < 6; j++) {
-      let tds = trs[j].querySelectorAll("td");
+      let tr = document.createElement("TR");
 
       for (let k = 0; k < 7; k++) {
-        let td = tds[k];
+        let td = document.createElement("TD");
         let dateTxt = document.createElement("div");
         dateTxt.setAttribute("class", "date_txt");
         td.appendChild(dateTxt);
@@ -90,29 +83,31 @@ document.addEventListener("DOMContentLoaded", () => {
         if (j === 0 && prevMonthDays != 7 && k < prevMonthDays) {
           dateTxt.textContent = dIndex.prev;
           if (valDay.month === 1) {
-            td.setAttribute(
-              "class",
-              `y_${valDay.year - 1} m_12 d_${dIndex.prev}`
-            );
+            td.classList.add(`y_${valDay.year - 1}`);
+            td.classList.add(`m_12`);
           } else {
-            td.setAttribute(
-              "class",
-              `y_${valDay.year} m_${valDay.month - 1} d_${dIndex.prev}`
-            );
+            td.classList.add(`y_${valDay.year}`);
+            td.classList.add(`m_${valDay.month - 1}`);
           }
+          td.classList.add(`d_${dIndex.prev}`);
           td.classList.add("prevMonth");
           dIndex.prev++;
 
           // 이번 달 날짜 표시
         } else if (dIndex.current <= lastDate) {
           dateTxt.textContent = dIndex.current;
-          td.setAttribute(
-            "class",
-            `y_${valDay.year} m_${valDay.month} d_${dIndex.current}`
-          );
+          td.classList.add(`y_${valDay.year}`);
+          td.classList.add(`m_${valDay.month}`);
+          td.classList.add(`d_${dIndex.current}`);
           const tdClassArr = Array.from(td.classList);
           if (matchToday(tdClassArr, todayArr)) {
             td.classList.add("today");
+
+            // schedule 임시 추가용
+            const schedule = document.createElement("div");
+            schedule.textContent = "schedule";
+            schedule.classList.add("schedule");
+            td.appendChild(schedule);
           }
           dIndex.current++;
 
@@ -120,20 +115,19 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
           dateTxt.textContent = dIndex.next;
           if (valDay.month === 12) {
-            td.setAttribute(
-              "class",
-              `y_${valDay.year + 1} m_1 d_${dIndex.next}`
-            );
+            td.classList.add(`y_${valDay.year + 1}`);
+            td.classList.add(`m_1`);
           } else {
-            td.setAttribute(
-              "class",
-              `y_${valDay.year} m_${valDay.month + 1} d_${dIndex.next}`
-            );
+            td.classList.add(`y_${valDay.year}`);
+            td.classList.add(`m_${valDay.month + 1}`);
           }
+          td.classList.add(`d_${dIndex.next}`);
           td.classList.add("nextMonth");
           dIndex.next++;
         }
+        tr.appendChild(td);
       } // 1주 for문 종료
+      tbody.appendChild(tr);
     } // 1달 for문 종료
 
     showNum();
@@ -164,13 +158,6 @@ document.addEventListener("DOMContentLoaded", () => {
     showDate();
   });
 
-  // search detail button 클릭하면 dropdown 표시, arrow 토글
-  btnSearchDetail?.addEventListener("click", () => {
-    document
-      .querySelector("div.btn_search_detail_arrow")
-      .classList.toggle("active");
-  });
-
   // 이벤트 버블링 이용, schedule 클릭 시 modal 창과 bgBlur 띄우기
   calendar?.addEventListener("click", (e) => {
     const target = e.target;
@@ -187,37 +174,15 @@ document.addEventListener("DOMContentLoaded", () => {
     modal.close();
   });
 
+  const bgImage = document.querySelector(".bg_image");
+
+  if (today.month === 11 || today.month <= 2) {
+    bgImage.style.backgroundImage = "url('../images/winter bg.png')";
+  }
+
   // 렌더링 완료 후 즉시 실행
   showDate();
 });
-
-// 월별 배경 변경 switch문
-// switch (valDay.month) {
-//   case 1:
-//     break;
-//   case 2:
-//     break;
-//   case 3:
-//     break;
-//   case 4:
-//     break;
-//   case 5:
-//     break;
-//   case 6:
-//     break;
-//   case 7:
-//     break;
-//   case 8:
-//     break;
-//   case 9:
-//     break;
-//   case 10:
-//     break;
-//   case 11:
-//     break;
-//   case 12:
-//     break;
-// }
 
 /**
  * TO DO
--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const btnSearch = document.querySelector("button.btn_search_detail");
+  const btnSearchDetail = document.querySelector("button.btn_search_detail");
   const searchDropdownBox = document.querySelector(
-    "div.search_detail_dropdown_box"
+    ".search_detail_dropdown_box"
   );
 
-  btnSearch?.addEventListener("click", () => {
+  // search detail button 클릭하면 dropdown 표시, arrow 토글
+  btnSearchDetail?.addEventListener("click", () => {
     searchDropdownBox.classList.toggle("visible");
+    document
+      .querySelector(".btn_search_detail_arrow")
+      .classList.toggle("active");
   });
 });
