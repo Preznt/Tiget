eb3241b2e25c9fad43c7c04383a5f6daf132a4e1
preznt <bjw1403@gmail.com>
2022-11-22 01:48:25 +0900

    swiper & ranking

--- Main/routes/index.js
+++ Main/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "callor.com Express" });
+  res.render("index");
 });
 
 export default router;
