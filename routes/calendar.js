d341a19c45970b406611f980a5ebfb6cd0d979dc
PollyGotACracker <bitterns96@gmail.com>
2022-11-22 12:58:46 +0900

    경로 수정

---
+++ routes/calendar.js
@@ -0,0 +1,19 @@
+import express from "express";
+import DB from "../models/index.js";
+const Holiday = DB.models.tbl_holiday;
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  const holidays = await Holiday.findAll();
+  const holiArr = [];
+  // for (let i of holidays) {
+  //   holiArr.push(i.dataValues);
+  // }
+  // console.log(holidays);
+  // res.json(holidays);
+  //
+  res.render("calendar", { holidays: Promise.all(holidays) });
+});
+
+export default router;
