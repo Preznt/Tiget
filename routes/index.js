81f1c603aaac45de7f8ff6cc4a06adb5a92db522
Preznt <bjw1403@gmail.com>
2022-11-22 15:02:27 +0900

    파일 병합

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("main");
 });
 
 export default router;
