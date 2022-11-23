6888a233e253a223a2f6215baa597887adedf69d
Preznt <bjw1403@gmail.com>
2022-11-23 17:06:16 +0900

    배경

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rankBox = document.querySelector("div.ranking");
+  const mainHeader = document.querySelector("header.main");
 
-  rankBox.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.tagName === "BUTTON") {
-    }
-  });
+  // rankBox?.addEventListener("click", (e) => {
+  //   const target = e.target;
+  //   if (target.tagName === "BUTTON") {
+  //   }
+  // });
 
-  window.addEventListener("scroll", () => {
-    let top =
-      window.scrollY ||
-      window.pageXOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
+  window?.addEventListener("scroll", () => {
+    const top = document.body.scrollTop;
 
     if (top > 50) {
-      fixedNav.classList.add("active");
+      mainHeader.classList.add("active");
     } else {
-      fixedNav.classList.remove("active");
+      mainHeader.classList.remove("active");
     }
   });
+
+  // window.onscroll = scrollF();
+
+  // function scrollF() {
+  //   if (document.body.scrollTop > 50) {
+  //     mainHeader.classList.add("active");
+  //   } else {
+  //     mainHeader.classList.remove("active");
+  //   }
+  // }
 });
