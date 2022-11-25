1c5fc6f4d71ded97cbdb0be92cd99aeba9217a7b
SoonhanSaram <kyengmin911@naver.com>
2022-11-25 10:46:39 +0900

    11-25

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rankBox = document.querySelector("div.ranking");
   const mainHeader = document.querySelector("header.main");
+  const pageView1 = document.querySelector("div.ranking");
+  const pageView2 = document.querySelector("section.calendar.container");
+  const pageView3 = document.querySelector("section.main3");
+  const navButtons = document.querySelector("div.main.button.box button");
 
+  navButtons?.addEventListener("click", (e) => {
+    const button = e.target;
+    const btnClass = button.textContent;
+    if (btnClass == "캘린더") {
+      let location = pageView1.offsetTop;
+      window.scrollTo({ top: location - 100, behavior: "smooth" });
+    }
+  });
   // rankBox?.addEventListener("click", (e) => {
   //   const target = e.target;
   //   if (target.tagName === "BUTTON") {
