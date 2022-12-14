466647ac6a9ceabf8e2e639c97c809df93a528ed
Preznt <bjw1403@gmail.com>
2022-12-14 16:42:40 +0900

    페이지네이션 구현중

--- public/js/concert.js
+++ public/js/concert.js
@@ -81,13 +81,13 @@ document.addEventListener("DOMContentLoaded", () => {
       ).href = `http://localhost:3002/detail/${concert[i].concert_code}`;
     }
   };
+
   headerBox?.addEventListener("click", async (e) => {
     const event = e.target;
     if (event.tagName === "BUTTON") {
       btnActive(headerBtns, event);
       const response = await fetch(`/concert/${event.textContent}`);
       const resJson = await response.json();
-
       cDataActive(resJson);
       bannerActive(resJson);
     }
@@ -111,4 +111,85 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((region) => rDataActive(region));
     }
   });
+
+  // 페이지네인션 구현중
+  const contents = document.querySelector("article.concert.info ul.contents");
+  const buttons = document.querySelector("article.concert.info div.buttons");
+
+  const numOfContent = 40;
+  const maxContent = 10;
+  const maxButton = 4;
+  const maxPage = Math.ceil(numOfContent / maxContent);
+  let page = 1;
+
+  const makeContent = (id, region) => {
+    const content = document.createElement("a");
+    content.classList.add("content");
+    content.innerHTML = `
+      <img src= ${region[id - 1].concert_poster}/>
+      <div class="content__title">${region[id - 1].concert_name}</span>
+      <div class="content__name">${region[id - 1].concert_loc}</span>
+      <div class="content__date">2022.01.01</span>
+    `;
+    return content;
+  };
+
+  const makeButton = (id) => {
+    const button = document.createElement("button");
+    button.classList.add("button");
+    button.dataset.num = id;
+    button.innerText = id;
+    button.addEventListener("click", (e) => {
+      Array.prototype.forEach.call(buttons.children, (button) => {
+        if (button.dataset.num) button.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      renderContent(parseInt(e.target.dataset.num));
+    });
+    return button;
+  };
+
+  const renderContent = (page) => {
+    // 목록 리스트 초기화
+    while (contents.hasChildNodes()) {
+      contents.removeChild(contents.lastChild);
+    }
+
+    // 글의 최대 개수를 넘지 않는 선에서, 화면에 최대 10개의 글 생성
+    for (
+      let id = (page - 1) * maxContent + 1;
+      id <= page * maxContent && id <= numOfContent;
+      id++
+    ) {
+      fetch(`/concert/국내`)
+        .then((res) => res.json())
+        .then((region) => contents.appendChild(makeContent(id, region)));
+    }
+  };
+
+  const renderButton = (page) => {
+    // 버튼 리스트 초기화
+    while (buttons.hasChildNodes()) {
+      buttons.removeChild(buttons.lastChild);
+    }
+    // 화면에 최대 5개의 페이지 버튼 생성
+    for (let id = page; id < page + maxButton && id <= maxPage; id++) {
+      buttons.appendChild(makeButton(id));
+    }
+    // 첫 버튼 활성화(class="active")
+    buttons.children[0].classList.add("active");
+
+    buttons.prepend(prev);
+    buttons.append(next);
+
+    // 이전, 다음 페이지 버튼이 필요한지 체크
+    if (page - maxButton < 1) buttons.removeChild(prev);
+    if (page + maxButton > maxPage) buttons.removeChild(next);
+  };
+
+  const render = (page) => {
+    renderContent(page);
+    renderButton(page);
+  };
+  render(page);
 });
